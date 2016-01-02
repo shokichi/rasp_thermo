d9999db88972452ed04d6b40269bafe2480af74f
ISHIOKA Sho <eee.viajero730@gmail.com>
2016-01-02 17:42:01 +0900

    be smart

--- hmz/humidity.rb
+++ hmz/humidity.rb
@@ -9,28 +9,30 @@ include Math
 
 =begin
 Reference data 
-    |   10,   15,   20,   25,   30,   35,   40
- ------------------------------------------------
- 20 | 0.75, 0.72, 0.69, 0.66, 0.62, 0.59, 0.55
- 30 | 1.03, 1.00, 1.00, 0.99, 0.96, 0.93, 0.90
- 40 | 1.32, 1.30, 1.31, 1.32, 1.30, 1.28, 1.25
- 50 | 1.64, 1.63, 1.64, 1.65, 1.64, 1.63, 1.61
- 60 | 1.97, 1.97, 1.98, 1.98, 1.98, 1.98, 1.96
- 70 | 2.30, 2.30, 2.31, 2.31, 2.31, 2.31, 2.30
- 80 | 2.64, 2.64, 2.63, 2.64, 2.63, 2.63, 2.61
- 90 | 2.97, 2.96, 2.94, 2.97, 2.94, 2.92, 2.90 
+|   10,   15,   20,   25,   30,   35,   40
+------------------------------------------------
+20 | 0.75, 0.72, 0.69, 0.66, 0.62, 0.59, 0.55
+30 | 1.03, 1.00, 1.00, 0.99, 0.96, 0.93, 0.90
+40 | 1.32, 1.30, 1.31, 1.32, 1.30, 1.28, 1.25
+50 | 1.64, 1.63, 1.64, 1.65, 1.64, 1.63, 1.61
+60 | 1.97, 1.97, 1.98, 1.98, 1.98, 1.98, 1.96
+70 | 2.30, 2.30, 2.31, 2.31, 2.31, 2.31, 2.30
+80 | 2.64, 2.64, 2.63, 2.64, 2.63, 2.63, 2.61
+90 | 2.97, 2.96, 2.94, 2.97, 2.94, 2.92, 2.90 
 
 =end
 
 OPT={}
-OPT[:test] = false
+
 opt=OptionParser.new
 
 opt.on("--test"){OPT[:test] = true}
+opt.on("--tmep=[value]"){|v| OPT[:temp] = v}
+opt.on("--volt=[value]"){|v| OPT[:volt] = v}
 opt.parse!(ARGV)
 
-
-ref_data = {
+# reference data
+RefHumidity = {
   temperature:[10, 15, 20, 25, 30, 35, 40],
   humidity:[20, 30, 40, 50, 60, 70, 80, 90],
   "10"=>[0.75, 1.03, 1.32, 1.64, 1.97, 2.30, 2.64, 2.97],
@@ -52,17 +54,19 @@ def linear_estimate(y, x1, y1, x2, y2)
 end
 
 def gen_pdev_vect(x1=[],x2=[],w1,w2)
+  
   return 0 if x1.size != x2.size 
 
   result = []
   x1.size.times do |n|
     result << (x1[n]*w2 + x2[n]*w1)/(w1 + w2)
   end
-  
+
   return result 
 end
 
 def search_near_index(ref=[],val)
+
   result = []
 
   (ref.size-1).times do |n|
@@ -95,22 +99,28 @@ end
 
 def volt2humidity_hmz(volt,temp)
   # Main
-  ref_temp = search_near_value(ref_data[:temperature], temp)
+  ref_temp = search_near_value(RefHumidity[:temperature], temp)
 
   ratio = calc_ratio(temp,ref_temp[0],ref_temp[1])
 
-  ref_volt_vect = gen_pdev_vect(ref_data[ref_temp[0].to_s], ref_data[ref_temp[1].to_s],ratio[0],ratio[1])
+  ref_volt_vect = gen_pdev_vect(RefHumidity[ref_temp[0].to_s], RefHumidity[ref_temp[1].to_s],ratio[0],ratio[1])
 
   hum1, hum2 = search_near_index(ref_volt_vect,volt)
 
-  return linear_estimate(volt,ref_data[:humidity][hum1],ref_volt_vect[hum1],ref_data[:humidity][hum2],ref_volt_vect[hum2])
-    
+  return linear_estimate(volt,RefHumidity[:humidity][hum1],ref_volt_vect[hum1],RefHumidity[:humidity][hum2],ref_volt_vect[hum2])
+
 end
 
+#################################################################
 
-# Main
-if OPT[:test]
+if OPT.has_key? :test
   temp = 15.0
   volt = 1.00
-  volt2humidity_hmz(volt,temp)
+  puts volt2humidity_hmz(volt,temp)
+elsif OPT.has_key? :temp and OPT.has_key? :volt
+  volt2humidity_hmz(OPT[:volt],OPT[:temp])
+else
+  puts "*** USAGE ***"
+  puts "./humidity.rb --temp=[value] --volt=[value]"
 end
+
